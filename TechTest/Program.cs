9485552d27b1c2e8daa1fcced6e23608ec98c4c7
adds137 <adds99@yahoo.com.au>
2025-03-30 18:45:08 +1100

    base solution impl

--- TechTest/Program.cs
+++ TechTest/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] example1 = new int[] {3, 0, 1};
+        int[] example2 = new int[] {9, 6, 4, 2, 3, 5, 7, 0, 1};
+
+        int result1 = Solution.FindMissingNumbers(example1);
+        int result2 = Solution.FindMissingNumbers(example2);
+
+        Console.WriteLine($"example 1 excepted answer: 2, actual answer: {result1}");
+        Console.WriteLine($"example 2 excepted answer: 8, actual answer: {result2}");
     }
 }
---
+++ TechTest/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace TechTest;
+
+public class Solution
+{
+    public static int FindMissingNumbers(int[] numberArray)
+    {
+        for (int i = 0; i < numberArray.Length; i++)
+        {
+            if (!numberArray.Contains(i))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
